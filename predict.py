5937aaddc2aa5b9b2362c4e252f64d87b0c59537
Naim Prottay <naimbinmasud@gmail.com>
2026-06-23 12:06:17 +0600

    Update Predict File

--- predict.py
+++ predict.py
@@ -1,41 +1,43 @@
 import joblib
-import pandas as pd
 import numpy as np
+import pandas as pd
 from feature_pipeline import banking_feature_eng
 
-
+# ১. মডেল এবং প্রসেসড ফাইলগুলো লোড করা
 model = joblib.load("bank_churn_predictor.pkl")
 threshold = joblib.load("threshold.pkl")
 mean_balance = joblib.load("mean_balance.pkl")
 feature_columns = joblib.load("feature_columns.pkl")
 
+# ২. কাস্টমার ডেটা (DataFrame)
+new_customer = pd.DataFrame(
+    {
+        "credit_score": [650],
+        "country": ["Germany"],
+        "gender": ["Male"],
+        "age": [45],
+        "tenure": [5],
+        "balance": [80000],
+        "products_number": [2],
+        "credit_card": [1],
+        "active_member": [1],
+        "estimated_salary": [70000],
+    }
+)
 
-new_customer = pd.DataFrame({
-    "credit_score": [650],
-    "country": ["Germany"],
-    "gender": ["Male"],
-    "age": [45],
-    "tenure": [5],
-    "balance": [80000],
-    "products_number": [2],
-    "credit_card": [1],
-    "active_member": [1],
-    "estimated_salary": [70000]
-})
-
-
-
+#feature engineering pipeline
 new_customer = banking_feature_eng(new_customer)
 
-for col in feature_columns:
-    if col not in new_customer.columns:
-        new_customer[col] = 0
+new_customer_ordered = new_customer.reindex(columns=feature_columns, fill_value=0)
 
-new_customer = new_customer[feature_columns]
-new_customer["is_welthy_active"] = ((new_customer["balance"] > mean_balance) & (new_customer["active_member"] == 1)).astype(int)
+#new binary feature include
+new_customer_ordered["is_welthy_active"] = (
+    (new_customer_ordered["balance"] > mean_balance)
+    & (new_customer_ordered["active_member"] == 1)
+).astype(int)
 
-prob = model.predict_proba(new_customer.values)[:,1][0]
-
+#direct pass dataframe into model
+prob = model.predict_proba(new_customer_ordered)[:, 1][0]
 
 print(f"Churn Probability: {prob:.4f}")
 
